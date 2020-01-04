134c057b46cc48b516306eb0e9ab4afda59b3808
fukuta2003 <alessandro.fukuta@etec.sp.gov.br>
2020-01-04 18:33:04 -0300

    PROJETO INICIAL SALARIO DOS FUNCIONARIOS

---
+++ SalarioFuncionariosListas/Employess.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace SalarioFuncionariosListas {
+    class Employess {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public string Situation { get; set; }
+
+        public Employess(int id, string name, double salary) {
+            Id = id;
+            Name = name;
+            Salary = salary;
+            Situation = "admitted";
+        }
+
+        public void increaseSalary(double percentage) {
+            Salary += ((Salary * percentage) / 100);
+        }
+
+        public void fired() {
+            Situation = "Fired";
+        }
+
+        public override string ToString() {
+            return Id
+                + ", "
+                + Name
+                + ", "
+                + Salary.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Situation;
+        }
+    }
+}
---
+++ SalarioFuncionariosListas/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+
+namespace SalarioFuncionariosListas {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("How many employess will be registered:");
+            int qt = int.Parse(Console.ReadLine());
+
+            int id = 0;
+            string name = "";
+            double salary = 0;
+           
+            List<Employess> Lista = new List<Employess>();
+
+            for (int i=0; i < qt; i++) {
+                Console.WriteLine("Employee #{0}",i+1);
+                Console.Write("ID.........:");
+                id = int.Parse(Console.ReadLine());
+                Console.Write("Name.......:");
+                name = Console.ReadLine();
+                Console.Write("Salary.....:");
+                salary = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+               
+                Lista.Add(new Employess (id, name, salary));
+            }
+            
+            Console.WriteLine("-----------------------------");
+
+            Console.WriteLine("Enter the employed ID that will have salary increase:");
+            int xReg = int.Parse(Console.ReadLine());
+
+            Employess emp = Lista.Find(x => x.Id == xReg);
+            if (emp != null) {
+                Console.Write("Enter with percentage:");
+                double perc = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                emp.increaseSalary(perc);
+            } else {
+                Console.WriteLine("employeed id not exists");
+            }
+
+
+            Console.WriteLine("Enter the employed ID that will have FIRED:");
+            int xFI = int.Parse(Console.ReadLine());
+            emp = Lista.Find(x => x.Id == xFI);
+            if (emp != null) {
+                emp.fired();
+            }
+            else {
+                Console.WriteLine("employeed id not exists");
+            }
+
+
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("REGISTERED EMPLOYESS");
+            foreach (Employess obj in Lista) {
+                Console.WriteLine(obj);
+            }
+
+
+
+
+        }
+    }
+}
